6391731ecb4544a81884096fbfddff8ef79c86f7
Ottorio <andrzej.gawron@interia.eu>
2024-12-24 23:05:53 +0100

    made selenium works

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from selenium import webdriver # pip3 install selenium
-# from selenium.webdriver import FirefoxOptions
 from selenium.webdriver.firefox.service import Service as FirefoxService # pip3 install webdriver_manager
 from webdriver_manager.firefox import GeckoDriverManager
 import time
 
-# opts = FirefoxOptions()
-# opts.add_argument("--headless")
-browser = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+address = "https://www.boot.dev"
+# address = "google.com"
 
-# address = "https://www.boot.dev/lessons/acb5117d-0f34-40b3-81f0-b89828f0e443"
-address = "google.com"
+# export TMPDIR=$HOME/tmp geckodriver
 
-browser.get(address)
+# import tempfile
+# tempfile.tempdir = '/home/user/my_tmp'
+
+
+driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+
+driver.get(address)
